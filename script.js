b5491e12cad06deb24b4ebd9dddd4e7a3c0c50e9
akshay-ghelani <akshayghelani627@gmail.com>
2025-03-09 14:10:57 +0530

    smiley game

--- script.js
+++ script.js
@@ -118,12 +118,80 @@ const ctx = canvas.getContext('2d');
 
 // animation 2 - character
 
+const image = document.getElementById("source");
 const player ={
     w: 50,
-    h: 70,
+    h: 50,
     x: 20,
     y: 200,
     speed: 5,
     dx: 0,
     dy: 0
 };
+function drawPlayer(){ 
+    ctx.drawImage(image, player.x,player.y,player.w,player.h);
+
+}
+
+function clear(){
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+}
+function newpostion(){
+    player.x += player.dx;
+    player.y += player.dy;
+}
+
+function update(){
+    drawPlayer();
+
+    newpostion();
+    requestAnimationFrame(update);
+}
+function moveUp(){
+    player.dy = -player.speed;
+}
+function moveDown() {
+    player.dy = player.speed;
+  }
+  
+  function moveRight() {
+    player.dx = player.speed;
+  }
+  
+  function moveLeft() {
+    player.dx = -player.speed;
+  }
+
+
+function keyDown(e) {
+    if (e.key === 'ArrowRight' || e.key === 'Right') {
+      moveRight();
+    } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+      moveLeft();
+    } else if (e.key === 'ArrowUp' || e.key === 'Up') {
+      moveUp();
+    } else if (e.key === 'ArrowDown' || e.key === 'Down') {
+      moveDown();
+    }
+  }
+
+function keyUp(e){
+    if (
+        e.key == 'Right' ||
+        e.key == 'ArrowRight' ||
+        e.key == 'Left' ||
+        e.key == 'ArrowLeft' ||
+        e.key == 'Up' ||
+        e.key == 'ArrowUp' ||
+        e.key == 'Down' ||
+        e.key == 'ArrowDown'
+      ) {
+        player.dx = 0;
+        player.dy = 0;
+      }
+}
+
+update();
+
+document.addEventListener('keydown',keyDown);
+document.addEventListener('keyup',keyUp);
